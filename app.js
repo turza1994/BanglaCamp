2fb885a9a1dd5d37b04f088a8018fc965351172b
turza1994 <turza1994@yahoo.com>
2023-02-25 19:09:22 +0600

    adding gitignore file, deleting node_modules folder

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 //=========================================
 //Configuring express and mongoose starts
 //=========================================
-var express         = require('express');
-var app             = express();
-var bodyParser      = require('body-parser');
-var mongoose        = require('mongoose');
-var flash           = require("connect-flash");
-var passport        = require('passport');
-var LocalStrategy   = require('passport-local');
-var methodOverride  = require('method-override');
-var seedDB          = require('./seeds');
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var flash = require('connect-flash')
+var passport = require('passport')
+var LocalStrategy = require('passport-local')
+var methodOverride = require('method-override')
+var seedDB = require('./seeds')
 
-var Comment         = require("./models/comment");
-var Camp            = require("./models/campground");
-var User            = require("./models/user");
+var Comment = require('./models/comment')
+var Camp = require('./models/campground')
+var User = require('./models/user')
 
-var indexRoutes     = require("./routes/index");
-var campgroundRoutes= require("./routes/campgrounds");
-var commentRoutes   = require("./routes/comments");
+var indexRoutes = require('./routes/index')
+var campgroundRoutes = require('./routes/campgrounds')
+var commentRoutes = require('./routes/comments')
 //  seedDB();
 
 // mongoose.connect("mongodb://localhost/yelp-camp");
@@ -36,52 +36,53 @@ var commentRoutes   = require("./routes/comments");
 //     useUnifiedTopology: true
 // });
 
-mongoose.connect("mongodb+srv://turza:12345@yelpcamp.usdgg.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(
+  'mongodb+srv://turza:12345@yelpcamp.usdgg.mongodb.net/test?retryWrites=true&w=majority',
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+)
 
-
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(methodOverride("_method"));
-app.use(flash());
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(methodOverride('_method'))
+app.use(flash())
 //=========================================
 //Configuring express and mongoose ends
 //=========================================
 
-
 //=============================
 //passport configuration starts
 //=============================
-app.use(require('express-session')({
-    secret: "i love gixxer",
+app.use(
+  require('express-session')({
+    secret: 'i love gixxer',
     resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+    saveUninitialized: false,
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 //===========================
 //passport configuration ends
 //===========================
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  res.locals.error = req.flash('error')
+  res.locals.success = req.flash('success')
+  next()
+})
 
-app.use((req,res,next)=>{
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
-});
+app.use(indexRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/comments', commentRoutes)
 
-app.use(indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
-
-app.listen(process.env.PORT || 3000, process.env.IP, ()=>{
-    console.log("server has been started");
-});
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+  console.log('server has been started')
+})
--- embed.js
+++ embed.js
@@ -1,37 +1,37 @@
-var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/data_model");
+// var mongoose = require('mongoose');
+// mongoose.connect("mongodb://localhost/data_model");
 
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
-var Post = mongoose.model('Post',postSchema);
+// var postSchema = new mongoose.Schema({
+//     title: String,
+//     content: String
+// });
+// var Post = mongoose.model('Post',postSchema);
 
-var userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    posts: [postSchema]
-});
-var User = mongoose.model('User',userSchema);
+// var userSchema = new mongoose.Schema({
+//     name: String,
+//     email: String,
+//     posts: [postSchema]
+// });
+// var User = mongoose.model('User',userSchema);
 
-User.findById("5de38dc19326df22c847ea85", (err, user)=>{
-    if(err){
-        console.log(err);
-    } else{
-        // console.log(user);
-        user.posts.push({
-            title: "Turza's bike",
-            content: "He loves yamaha fz-s"
-        });
-        user.save((err, user)=>{
-            if(err){
-                console.log(err);
-            } else{
-                console.log(user);
-            }
-        });
-    }
-});
+// User.findById("5de38dc19326df22c847ea85", (err, user)=>{
+//     if(err){
+//         console.log(err);
+//     } else{
+//         // console.log(user);
+//         user.posts.push({
+//             title: "Turza's bike",
+//             content: "He loves yamaha fz-s"
+//         });
+//         user.save((err, user)=>{
+//             if(err){
+//                 console.log(err);
+//             } else{
+//                 console.log(user);
+//             }
+//         });
+//     }
+// });
 
 // var newUser = new User({
 //     name: "Turza",
@@ -48,4 +48,3 @@ User.findById("5de38dc19326df22c847ea85", (err, user)=>{
 //         console.log(user);
 //     }
 // });
-
